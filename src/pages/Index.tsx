cbe4300e4e2997236b6582424dec3f9c96ba3416
fortalezacertificados-ai <fortalezacertificados@gmail.com>
2025-09-28 16:19:26 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,7 +16,7 @@ const Index = () => {
             <img 
               src={estatuaImage} 
               alt="Estátua" 
-              className="absolute left-4 h-22 md:h-20 opacity-65 hover:opacity-95 transition-opacity duration-300"
+              className="absolute left-4 h-25 md:h-20 opacity-65 hover:opacity-95 transition-opacity duration-300"
             />
           </div>
         </div>
